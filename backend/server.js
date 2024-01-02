abdb77f1437349000ef89b99ebc3366f0d5d711b
Sushant Chalise <susantchalise@gmail.com>
2024-01-02 23:56:56 +0545

    server created

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+app.get('/',(req,res)=>{
+    res.send("API is running")
+})
+
+//app.get()
+const PORT = process.config.PORT || 3000
+
+app.listen(PORT,console.log(`Server started on port ${PORT}`))
