f18c60eff5338d96ca98adf3165b178503a16d58
Sushant Chalise <susantchalise@gmail.com>
2024-01-06 23:53:39 +0545

    Added chakra icons and completed sign up UI

--- frontend/src/components/Auth/Signup.js
+++ frontend/src/components/Auth/Signup.js
@@ -1,11 +1,28 @@
-import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 
 const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [show, setshow] = useState(false);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const postDetails = () => {};
+  const submitHandler = () => {};
   return (
     <VStack spacing="5px">
       <FormControl>
@@ -24,20 +41,56 @@ const Signup = () => {
       </FormControl>
       <FormControl>
         <FormLabel>Password</FormLabel>
-        <Input
-          placeholder="Enter Password"
-          onChange={(e) => setName(e.target.value)}
-          type="password"
-        />
+        <InputGroup>
+          <Input
+            placeholder="Enter Password"
+            onChange={(e) => setName(e.target.value)}
+            type={show ? "text" : "password"}
+          />
+          <InputRightElement width="4.5rem" cursor="pointer">
+            {show ? (
+              <ViewIcon onClick={handleClick} />
+            ) : (
+              <ViewOffIcon onClick={handleClick} />
+            )}
+          </InputRightElement>
+        </InputGroup>
       </FormControl>
       <FormControl>
         <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            placeholder="Enter Confirmation Password"
+            onChange={(e) => setName(e.target.value)}
+            type="password"
+          />
+          <InputRightElement width="4.5rem" cursor="pointer">
+            {show ? (
+              <ViewIcon onClick={handleClick} />
+            ) : (
+              <ViewOffIcon onClick={handleClick} />
+            )}
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="picture">
+        <FormLabel>Upload your Picture</FormLabel>
         <Input
-          placeholder="Enter Confirmation Password"
-          onChange={(e) => setName(e.target.value)}
-          type="password"
+          type="file"
+          p="1.5"
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
         />
       </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        color="white"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
     </VStack>
   );
 };
