c151ace49c1418eb838b403e39c87e9589142ea9
Sushant Chalise <susantchalise@gmail.com>
2024-01-06 23:58:23 +0545

    Added Login UI

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,7 +1,65 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px">
+      <FormControl>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+            type={show ? "text" : "password"}
+          />
+          <InputRightElement width="4.5rem" cursor="pointer">
+            {show ? (
+              <ViewIcon onClick={handleClick} />
+            ) : (
+              <ViewOffIcon onClick={handleClick} />
+            )}
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        color="white"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
