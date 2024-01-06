efd2267532f89a07391a8f60045120eb56392a5a
Sushant Chalise <susantchalise@gmail.com>
2024-01-07 00:40:43 +0545

    Added the routes for login and register

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,9 +12,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 const PORT = process.config.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
