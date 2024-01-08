e773448c50fe543a94c5221e251586a064415615
Sushant Chalise <susantchalise@gmail.com>
2024-01-08 23:24:08 +0545

    Error handler middleware added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -16,7 +17,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 
-app.
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.config.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
