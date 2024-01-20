38a67f38683aa3a91f6316e0f746e95b53452709
Sushant Chalise <susantchalise@gmail.com>
2024-01-21 01:21:31 +0545

    implemented signup in the frontend

--- frontend/src/components/Auth/Signup.js
+++ frontend/src/components/Auth/Signup.js
@@ -6,9 +6,13 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
   const [name, setName] = useState();
@@ -16,13 +20,98 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [show, setShow] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  const [pic, setPic] = useState();
+  const toast = useToast();
+  const navigate = useNavigate();
   const handleClick = () => {
     setShow(!show);
   };
 
-  const postDetails = () => {};
-  const submitHandler = () => {};
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "do3qr05ry");
+      fetch("https://api.cloudinary.com/v1_1/do3qr05ry/image/upload", {
+        method: "Post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "botton",
+      });
+      setLoading(false);
+      return;
+    }
+  };
+  const submitHandler = async () => {
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Enter in all the fields",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        const { data } = await axios.post(
+          "/api/user",
+          { name, email, password, pic },
+          config
+        );
+        toast({
+          title: "Registration is Successful",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+        navigate("/chat");
+      } catch (err) {}
+    }
+  };
   return (
     <VStack spacing="5px">
       <FormControl>
@@ -36,7 +125,7 @@ const Signup = () => {
         <FormLabel>Email</FormLabel>
         <Input
           placeholder="Enter Your Email"
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </FormControl>
       <FormControl>
@@ -44,7 +133,7 @@ const Signup = () => {
         <InputGroup>
           <Input
             placeholder="Enter Password"
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             type={show ? "text" : "password"}
           />
           <InputRightElement width="4.5rem" cursor="pointer">
@@ -61,7 +150,7 @@ const Signup = () => {
         <InputGroup>
           <Input
             placeholder="Enter Confirmation Password"
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => setConfirmpassword(e.target.value)}
             type="password"
           />
           <InputRightElement width="4.5rem" cursor="pointer">
@@ -88,6 +177,7 @@ const Signup = () => {
         color="white"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
